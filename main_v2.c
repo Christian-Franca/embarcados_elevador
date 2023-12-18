bab8ad224944ab8090ab38191e87196e3f26b8f2
joaojjvitor <joaovitordasilvafonseca@gmail.com>
2023-12-17 21:39:14 -0300

    Testando

---
+++ main_v2.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   main_v2.c
+ * Author: joaov
+ *
+ * Created on 17 de Dezembro de 2023, 21:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
+sdsa
+
+
+teste 
+teste dsadsadasdasd
+teste
